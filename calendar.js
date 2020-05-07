02328641a87c45eccf856fe895dbc9da074ee313
Aaron Geronimo <42796623+aaronjgeronimo@users.noreply.github.com>
2020-05-07 00:43:09 -1000

    Cleaner RRULE fix.

--- calendar.js
+++ calendar.js
@@ -21,12 +21,13 @@ function makeICS() {
     startTime = startTime.split(":");
     endTime = endTime.split(":");
 
-    //Work around for RRULE:FREQ=
-    if (rec == "") {
       var icsMSG =
           "BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//Our Company//NONSGML v1.0//EN" +
           "\nBEGIN:VTIMEZONE" + "\nTZID:" + tZID + "\nEND:VTIMEZONE" +
-          "\nBEGIN:VEVENT\nUID:me@google.com\nDTSTAMP:" +
+          "\nBEGIN:VEVENT" +
+          "\nSUMMARY:" +
+          name +
+          "\nUID:me@google.com\nDTSTAMP:" +
           (startDate[2] + startDate[0] + startDate[1]) +
           "T" +
           startTime[0] +
@@ -50,72 +51,34 @@ function makeICS() {
           classification +
 
           "\nPRIORITY:" +
-          priority +
+          priority;
 
-          "" + "\nLOCATION:" +
-          location +
+          if (rec != "") {
+            icsMSG += "\nRRULE:" +
+                rec;
+          }
 
-          "\nGEO:" +
-          geoPosLAT +
-          ";" +
-          geoPosLONG +
+          if (location != "") {
+            icsMSG += "\nLOCATION:" +
+            location;
+          }
 
-          "\nSUMMARY:" +
-          name +
+          if (geoPosLAT != "" && geoPosLONG != "") {
+            icsMSG += "\nGEO:" +
+            geoPosLAT +
+            ";" +
+            geoPosLONG;
+          }
 
-          "\nDESCRIPTION:" +
-          description +
+          if (description != "") {
+            icsMSG += "\nDESCRIPTION:" +
+            description;
+          }
 
-          "\nEND:VEVENT\nEND:VCALENDAR";
-    } else {
-      var icsMSG =
-          "BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//Our Company//NONSGML v1.0//EN" +
-          "\nBEGIN:VTIMEZONE" + "\nTZID:" + tZID + "\nEND:VTIMEZONE" +
-          "\nBEGIN:VEVENT\nUID:me@google.com\nDTSTAMP:" +
-          (startDate[2] + startDate[0] + startDate[1]) +
-          "T" +
-          startTime[0] +
-          startTime[1] +
-          "00" +
-          "" +
-          "\nATTENDEE;CN=My Self ;RSVP=TRUE:MAILTO:me@gmail.com\nORGANIZER;CN=Me:MAILTO::" + sentBy + "\nDTSTART:" +
-          (startDate[0] + startDate[1] + startDate[2]) +
-          "T" +
-          startTime[0] +
-          startTime[1] +
-          "00" +
-          "" + "\nDTEND:" +
-          (endDate[0] + endDate[1] + endDate[2]) +
-          "T" +
-          endTime[0] +
-          endTime[1] +
-          "00" +
+          icsMSG += "\nEND:VEVENT\nEND:VCALENDAR";
 
-          "\nRRULE:" +
-          rec +
 
-          "\nCLASS:" +
-          classification +
 
-          "\nPRIORITY:" +
-          priority +
-
-          "" + "\nLOCATION:" +
-          location +
-
-          "\nGEO:" +
-          geoPosLAT +
-          ";" +
-          geoPosLONG +
-
-          "\nSUMMARY:" +
-          name +
-
-          "\nDESCRIPTION:" +
-          description +
-
-          "\nEND:VEVENT\nEND:VCALENDAR";
-    }
     var element = document.createElement("a");
     console.log(icsMSG);
 
